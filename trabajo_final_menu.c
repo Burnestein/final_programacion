c0a6c7faf50dd302e9e92806952621e66eb92e7c
Burnestein <burn_nemesis@hotmail.com>
2021-06-03 10:50:24 -0500

    main
    Se crea funcion main

---
+++ trabajo_final_menu.c
@@ -0,0 +1,16 @@
+/*
+PROGRAMACIÓN I
+Alumno: Nemesis J. Sierra Aranda 221404306
+Profesor: Hugo Adrián Delgado Rodriguez
+Carrera: Ingeniería en Computación, 1er Semestre
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+getchar();
+return 0;
+}
+
